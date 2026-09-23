94f5c5c0901e3921d1dfad9e2795d0515056f809
rpsv_codes <108035732+RedjanVisitacion@users.noreply.github.com>
2026-09-22 18:49:11 -0700

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,10 +1,10 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "group1_db");
+$conn = new mysqli("localhost", "root", "", "pentacode");
 if ($conn->connect_error) {
- die("Database connection failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 echo "<h1>IT412 Web and Database Server</h1>";
-echo "<h2>Groupname</h2>";
+echo "<h2>PentaCode</h2>";
 echo "<p>Database connection successful!</p>";
 $conn->close();
 ?>
